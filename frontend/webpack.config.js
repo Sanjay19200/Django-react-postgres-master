e77bc2dc464691298f4527e1af6d5285620a9cfd
Sanjay19200 <136919818+Sanjay19200@users.noreply.github.com>
2025-11-06 11:53:20 +0530

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -27,7 +27,7 @@ module: {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
+    publicPath: "/",
     filename: "bundle.js"
   },
   devServer: {
